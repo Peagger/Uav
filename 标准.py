b0e70d2d6e0a948f1b56c945548fcc91ed492ca4
pigger <1103745242@qq.com>
2022-09-19 19:51:57 +0800

    无人机移动模块

--- 标准.py
+++ 标准.py
@@ -1,15 +1,42 @@
-双黄列表=[]
-while('小猪'not in 双黄列表):
-    for 玩家名称 in 双黄列表:
-        print(玩家名称+'傻逼',end=' ')
-    print('')
-    玩家名称=input()
-    双黄列表.append(玩家名称)
-print(玩家名称+'聪明逼')
+# from turtle import position
+
+import copy
+import random
+import numpy as np
+# 双黄列表=[]
+# while('小猪'not in 双黄列表):
+#     for 玩家名称 in 双黄列表:
+#         print(玩家名称+'傻逼',end=' ')
+#     print('')
+#     玩家名称=input()
+#     双黄列表.append(玩家名称)
+# print(玩家名称+'聪明逼')
 
 
-class Vertix():
-    def __init__(self) -> None:
-        self.occupy=None
-        self.posision=[]
+class Uav():
+    def __init__(self,x,y,z) -> None:
+        self.x=x
+        self.y=y
+        self.z=z    #位置信息
+        self.speed=5    #无人机速度
+        self.in_place=False
+    
+    def move(self,angel_x,angel_y,angel_z):
+        '''向给定角度移动无人机'''
+        self.x+=self.speed*np.cos(angel_x)
+        self.y+=self.speed*np.cos(angel_y)
+        self.z+=self.speed*np.cos(angel_z)
+
+    
+    def printPos(self):
+        for i in self.position:
+            print(i,end=',')
+        print('\n')
+
+
+if __name__=='__main__':
+    position=[]
+    for i in range(4):
+        x,y,z=random.randint(0,100),random.randint(0,100),random.randint(0,100)
+        print(x,y,z)
         
